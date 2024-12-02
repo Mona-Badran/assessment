6b0a7dc0271cd075bcf9170f659a94943a90eaed
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-12-02 12:09:30 +0200

    added context in the projects model

--- laravel/app/Models/Project.php
+++ laravel/app/Models/Project.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
-}
+    use HasFactory;
+
+    /**
+     * Get the users associated with the project.
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'project_user', 'project_id', 'user_id');
+    }}
