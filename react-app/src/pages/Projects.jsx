96f486a291c0759c075878d77f7988d1ed6004b9
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-12-02 11:38:47 +0200

    added sample projects

--- react-app/src/pages/Projects.jsx
+++ react-app/src/pages/Projects.jsx
@@ -2,6 +2,26 @@ import React from "react";
 import Project from "../components/Project";
 
 const Projects = () => {
+  const projects = [
+    {
+      id: 1,
+      name: "Project A",
+      description: "Sample project.",
+      members: [
+        { id: 1, name: "Jane" },
+        { id: 2, name: "Taha" },
+      ],
+    },
+    {
+      id: 2,
+      name: "Project B",
+      description: "Sample project.",
+      members: [
+        { id: 3, name: "Charbel" },
+        { id: 4, name: "Nour" },
+      ],
+    },
+  ];
   return (
     <div className="projects-container">
       {projects.map((p) => (
