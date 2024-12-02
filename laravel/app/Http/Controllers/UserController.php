03b01073d7b5c2cf4971e4f09c7fca9c85aaff30
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-12-02 12:27:28 +0200

    Deleted a User

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -57,6 +57,18 @@ class UserController extends Controller
         return response()->json(['message' => 'User updated successfully', 'user' => $user]);
     
     }
+    public function delete($id){
+        $user = \App\Models\User::find($id);
+
+        if (!$user) {
+            return response()->json(['message' => 'User not found'], 404);
+        }
+    
+        $user->delete();
+    
+        return response()->json(['message' => 'User deleted successfully']);
+    }
+
 
 
 }
