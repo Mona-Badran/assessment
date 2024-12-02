deeac1005638d778843062bc984e2dae0dbfaaf4
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-12-02 12:28:21 +0200

    Fetched all projects

--- laravel/app/Http/Controllers/ProjectController.php
+++ laravel/app/Http/Controllers/ProjectController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(){
+        $projects = \App\Models\Project::all(); // Fetch all projects
+        return response()->json($projects);
+    }
 }
