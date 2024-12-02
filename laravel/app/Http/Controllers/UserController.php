64e47355173c54cb5cfe19b53395782cf2484906
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-12-02 12:24:48 +0200

    added index

--- laravel/app/Http/Controllers/UserController.php
+++ laravel/app/Http/Controllers/UserController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+        $users = \App\Models\User::all(); // Fetch all users
+        return response()->json($users); 
+    }
+
 }
