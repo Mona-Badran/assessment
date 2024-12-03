024f91ec6e094e711238b363422c988fbf014006
Mona-Badran <98732725+Mona-Badran@users.noreply.github.com>
2024-12-03 09:47:20 +0200

    added route to app.js

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Projects from "./pages/Projects";
+import UsersList from "./components/UsersList";
+
 
 function App() {
   return (
@@ -8,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/projects" element={<Projects />} />
+          <Route path="/users" element={<UsersList />} />
         </Routes>
       </BrowserRouter>
     </div>
